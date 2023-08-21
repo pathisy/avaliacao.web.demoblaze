c993a31d0e567a221afe63e9da1e67d9e5c2d2c3
Pathisy Pimentel <119361445+pathisy@users.noreply.github.com>
2023-08-21 16:10:18 -0300

    Update Login_Logout.java

--- WORKSPACE_AVALIACAO_DEMOBLAZE/avaliacao.web.demoblaze/src/main/java/avaliacao/web/demoblaze/Login_Logout.java
+++ WORKSPACE_AVALIACAO_DEMOBLAZE/avaliacao.web.demoblaze/src/main/java/avaliacao/web/demoblaze/Login_Logout.java
@@ -1,7 +1,5 @@
 package avaliacao.web.demoblaze;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.io.IOException;
 import org.apache.commons.io.FileUtils;
@@ -12,7 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import bases.DriverFactory;
 
 public class Login_Logout {
@@ -24,7 +22,7 @@ public class Login_Logout {
 	public void iniciar() {
 
 		url = ("https://www.demoblaze.com/");
-		driver = new ChromeDriver();
+		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 
 	}
@@ -32,13 +30,26 @@ public class Login_Logout {
 	@Test
 	public void login() throws InterruptedException {
 		driver.get(url);
+		Thread.sleep(1000);
+		driver.findElement(By.id("login2")).click();
+		driver.findElement(By.id("loginusername")).sendKeys("pathisy");
+		driver.findElement(By.id("loginpassword")).sendKeys("teste123");
+		driver.findElement(By.xpath("//*[@onclick = 'logIn()']")).click();
+
+	}
+	
+	@Test
+	public void logout() throws InterruptedException {
+		driver.get(url);
 		driver.findElement(By.id("login2")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.id("loginusername")).sendKeys("pathisy");
 		driver.findElement(By.id("loginpassword")).sendKeys("teste123");
-		driver.findElement(By.xpath("//*[@onclick='logIn()']")).click();
-		assertTrue(url, true);
-
+		driver.findElement(By.xpath("//*[@onclick = 'logIn()']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//*[@onclick = 'logOut()']")).click();
+		
+		
 	}
 
 	@After
